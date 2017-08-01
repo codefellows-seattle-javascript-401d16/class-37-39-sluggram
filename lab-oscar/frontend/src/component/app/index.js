754a8bd32f6af51166eaf367d9835903206abbfe
Oscar Cauich <ocauich@gravitypayments.com>
2017-08-01 15:14:21 -0700

    lander container component added and displaying basic scalfold

--- lab-oscar/frontend/src/component/app/index.js
+++ lab-oscar/frontend/src/component/app/index.js
@@ -1,52 +1,28 @@
 import React from 'react';
+import LandingContainer from '../landing-container';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 class App extends React.Component {
   render(){
     return (
       <div>
-        <h1>Hello World</h1>
+        <BrowserRouter>
+          <div>
+            <header>
+              <h1> Welcome to The App</h1>
+              <nav>
+                <ul>
+                  <li><Link to='/signup'>Sign Up</Link></li>
+                  <li><Link to='/login'>Login</Link></li>
+                </ul>
+              </nav>
+            </header>
+            <Route path='/:auth' component={LandingContainer} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
 }
 
 export default App;
-
-
-//
-//
-// import React from 'react'
-// import {Provider} from 'react-redux'
-// import {BrowserRouter, Route, Link} from 'react-router-dom'
-// import appStoreCreate from '../../lib/app-store-create.js'
-// import LandingContainer from '../landing-container'
-//
-// let store =  appStoreCreate()
-//
-// class App extends React.Component {
-//   render(){
-//     return (
-//       <div className='app'>
-//         <Provider store={store}>
-//           <BrowserRouter>
-//             <div>
-//               <header>
-//                 <h1> cool swee awesome yeeee </h1>
-//                 <nav>
-//                   <ul>
-//                     <li><Link to='/welcome/signup'> signup </Link> </li>
-//                     <li><Link to='/welcome/login'> login </Link> </li>
-//                   </ul>
-//                 </nav>
-//               </header>
-//
-//               <Route path='/welcome/:auth' component={LandingContainer} />
-//             </div>
-//           </BrowserRouter>
-//         </Provider>
-//       </div>
-//     )
-//   }
-// }
-//
-// export default App
