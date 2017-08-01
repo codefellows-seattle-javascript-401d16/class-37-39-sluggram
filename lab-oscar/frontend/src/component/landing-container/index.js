86f0a7f073f830f3aaec07bf8842edee3d58440f
Oscar Cauich <ocauich@gravitypayments.com>
2017-08-01 15:52:44 -0700

    refactored landing container to include the form component and the action creators

--- lab-oscar/frontend/src/component/landing-container/index.js
+++ lab-oscar/frontend/src/component/landing-container/index.js
@@ -1,52 +1,36 @@
 import React from 'react';
+import AuthForm from '../auth-form';
+import {connect} from 'react-redux';
+import * as util from '../../lib/util.js';
+import {signupRequest, loginRequest} from '../../action/auth-actions.js';
 
 
 class LandingContainer extends React.Component {
   render(){
+    let {params} = this.props.match;
+    let handleComplete = params.auth === 'login'
+      ? this.props.login
+      : this.props.signup;
     return(
-      <div><h4>Landing Container</h4></div>
-    )
+      <div>
+        <AuthForm
+          auth={params.auth}
+          onComplete={this.handleComplete}
+        />
+      </div>
+    );
   }
 }
 
-export default LandingContainer;
-//
-//
-//
-// import {connect} from 'react-redux'
-// import AuthForm from '../auth-form'
-// import * as util from '../../lib/util.js'
-// import {signupRequest, loginRequest} from '../../action/auth-actions.js'
-//
-// class LandingContainer extends React.Component {
-//   render(){
-//     let {params} = this.props.match
-//     console.log('history', this.props.history)
-//     let handleComplete = params.auth === 'login'
-//       ? this.props.login
-//       : this.props.signup
-//
-//     return (
-//       <div>
-//         <AuthForm
-//           auth={params.auth}
-//           onComplete={handleComplete}
-//           />
-//
-//       </div>
-//     )
-//   }
-// }
-//
-// let mapStateToProps = () => ({})
-// let mapDispatchToProps = (dispatch) => {
-//   return {
-//     signup: (user) => dispatch(signupRequest(user)),
-//     login: (user) => dispatch(loginRequest(user)),
-//   }
-// }
-//
-// export default connect(
-//   mapStateToProps,
-//   mapDispatchToProps
-// )(LandingContainer)
+let mapStateToProps = () => ({});
+let mapDispatchToProps = (dispatch) => {
+  return {
+    signup: (user) => dispatch(signupRequest(user)),
+    login: (user) => dispatch(loginRequest(user)),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LandingContainer);
