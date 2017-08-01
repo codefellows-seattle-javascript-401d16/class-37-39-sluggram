366c6ad9545578e27240c811f749ec2d2fa63c16
Matthew Parker <cadburylion@outlook.com>
2017-08-01 15:26:39 -0700

    Added actions, reducers, middleware, and app component; working

--- lab-matthew/src/component/app/index.js
+++ lab-matthew/src/component/app/index.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import {Provider} from 'react-redux'
 import {BrowserRouter, Route, Link} from 'react-router-dom'
-// import appStoreCreate from '../../lib/app-store-create.js'
+import appStoreCreate from '../../lib/app-store-create.js'
 // import LandingContainer from '../landing-container'
 
-// let store = appStoreCreate()
+let store = appStoreCreate()
 
 class App extends React.Component{
   render(){
     return(
       <div className='app'>
-        <BrowserRouter>
-          <div>
-            <header>
-              <h1> Are you still readin this? </h1>
-            </header>
-          </div>
-        </BrowserRouter>
+        <Provider store={store}>
+          <BrowserRouter>
+            <div>
+              <header>
+                <h1> Are you still readin this? </h1>
+              </header>
+            </div>
+          </BrowserRouter>
+        </Provider>
       </div>
     )
   }
