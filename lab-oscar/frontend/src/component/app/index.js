980cac69accbbded8e52e97ab182da261f210695
Oscar Cauich <ocauich@gravitypayments.com>
2017-08-01 15:34:15 -0700

    refactored the app component to bring in redux store component and reducer

--- lab-oscar/frontend/src/component/app/index.js
+++ lab-oscar/frontend/src/component/app/index.js
@@ -1,25 +1,31 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import LandingContainer from '../landing-container';
+import appStoreCreate from '../../lib/app-store-create';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
+
+let store =  appStoreCreate();
 
 class App extends React.Component {
   render(){
     return (
       <div>
-        <BrowserRouter>
-          <div>
-            <header>
-              <h1> Welcome to The App</h1>
-              <nav>
-                <ul>
-                  <li><Link to='/signup'>Sign Up</Link></li>
-                  <li><Link to='/login'>Login</Link></li>
-                </ul>
-              </nav>
-            </header>
-            <Route path='/:auth' component={LandingContainer} />
-          </div>
-        </BrowserRouter>
+        <Provider store={store}>
+          <BrowserRouter>
+            <div>
+              <header>
+                <h1> Welcome to The App</h1>
+                <nav>
+                  <ul>
+                    <li><Link to='/signup'>Sign Up</Link></li>
+                    <li><Link to='/login'>Login</Link></li>
+                  </ul>
+                </nav>
+              </header>
+              <Route path='/:auth' component={LandingContainer} />
+            </div>
+          </BrowserRouter>
+        </Provider>
       </div>
     );
   }
