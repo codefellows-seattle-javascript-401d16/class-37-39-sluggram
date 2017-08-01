8b0312748b8aab1f0d9b50c502d488211f6ea0ab
Matthew Parker <cadburylion@outlook.com>
2017-08-01 15:00:31 -0700

    Scaffold complete, posting to DOM

---
+++ lab-matthew/src/component/app/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Provider} from 'react-redux'
+import {BrowserRouter, Route, Link} from 'react-router-dom'
+// import appStoreCreate from '../../lib/app-store-create.js'
+// import LandingContainer from '../landing-container'
+
+// let store = appStoreCreate()
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className='app'>
+        <BrowserRouter>
+          <div>
+            <header>
+              <h1> Are you still readin this? </h1>
+            </header>
+          </div>
+        </BrowserRouter>
+      </div>
+    )
+  }
+}
+
+export default App
