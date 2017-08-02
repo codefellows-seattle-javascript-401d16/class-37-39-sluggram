41ec77630c098c4c6a806a70069dae21424d238f
Oscar Cauich <ocauich@gravitypayments.com>
2017-08-02 13:53:53 -0700

    refactored util.js to add the cookie reader funtion

--- lab-oscar/frontend/src/lib/util.js
+++ lab-oscar/frontend/src/lib/util.js
@@ -9,3 +9,14 @@ export const renderIf = (test, component) =>
 
 export const classToggler = (config) =>
   Object.keys(config).filter(key => config[key]).join(' ');
+
+export const readCookie = (name) => {
+  var nameEQ = name + '=';
+  var ca = document.cookie.split(';');
+  for(var i=0; i < ca.length; i++) {
+    var c = ca[i];
+    while(c.charAt(0)==' ') c = c.substring(1, c.length);
+    if(c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+};
