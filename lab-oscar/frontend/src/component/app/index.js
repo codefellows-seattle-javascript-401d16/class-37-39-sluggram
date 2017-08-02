978f9eec77ce54eb06060a8702784422d4cb1d72
Oscar Cauich <ocauich@gravitypayments.com>
2017-08-02 13:44:26 -0700

    refactored app component to remove provider and add connect method

--- lab-oscar/frontend/src/component/app/index.js
+++ lab-oscar/frontend/src/component/app/index.js
@@ -1,36 +1,51 @@
 import React from 'react';
-import './_app-header.scss';
-import {Provider} from 'react-redux';
-import LandingContainer from '../landing-container';
-import appStoreCreate from '../../lib/app-store-create';
+import {connect} from 'react-redux';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
+
+import './_app-header.scss';
+import * as util from '../../lib/util.js';
+import LandingContainer from '../landing-container';
+import {tokenSet} from '../../action/auth-actions.js';
+import appStoreCreate from '../../lib/app-store-create';
 
 let store =  appStoreCreate();
 
 class App extends React.Component {
+  componentDidMount(){
+    let token = util.readCookie('X-Sluggram-Token');
+    if(token){
+      this.props.tokenSet(token);
+    }
+  }
   render(){
     return (
       <div>
-        <Provider store={store}>
-          <BrowserRouter>
-            <div>
-              <header className='header-main'>
-                <Link to='/' alt='Home' className='header-logo'>
-                  <img src='https://cdn0.iconfinder.com/data/icons/free-social-media-set/24/ok-64.png'
-                  />
-                </Link>
-                <ul className='navigation-main'>
-                  <li className='navigation-link'><Link to='/signup' alt='Sign Up'>Sign Up</Link></li>
-                  <li className='navigation-link'><Link to='/login' alt='Log In'>Login</Link></li>
-                </ul>
-              </header>
-              <Route exact path='/:auth' component={LandingContainer} />
-            </div>
-          </BrowserRouter>
-        </Provider>
+        <BrowserRouter>
+          <div>
+            <header className='header-main'>
+              <Link to='/' alt='Home' className='header-logo'>
+                <img src='https://cdn0.iconfinder.com/data/icons/free-social-media-set/24/ok-64.png'
+                />
+              </Link>
+              <ul className='navigation-main'>
+                <li className='navigation-link'><Link to='/signup' alt='Sign Up'>Sign Up</Link></li>
+                <li className='navigation-link'><Link to='/login' alt='Log In'>Login</Link></li>
+              </ul>
+            </header>
+            <Route exact path='/:auth' component={LandingContainer} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
 }
 
-export default App;
+let mapStateToProps = (state) => ({
+  profile: state.profile,
+});
+
+let mapDispatchToProps = (dispatch) => ({
+  tokenSet: (token) => dispatch(tokenSet(token)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- lab-oscar/frontend/src/main.js
+++ lab-oscar/frontend/src/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {Provider} from 'react-redux';
 import App from './component/app';
-import appStoreCreate from '.lib/app-store-create.js';
+import appStoreCreate from './lib/app-store-create.js';
 
 let store = appStoreCreate();
 let AppContainer = () => {
