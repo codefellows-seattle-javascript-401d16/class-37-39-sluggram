d1c2ebd2ee95576ee22d6f83622a539d56b763ac
Scott <sdmccoy@gmail.com>
2017-08-02 07:25:02 -0700

    redux reporter built

--- lab37-scott/src/lib/app-store-create.js
+++ lab37-scott/src/lib/app-store-create.js
@@ -1,7 +1,7 @@
 import {createStore, applyMiddleware} from 'redux';
 import combineReducer from '../reducer/combine-reducer.js';
+import reduxReporter from './redux-reporter.js';
 
-
-let appStoreCreate = () => createStore(combineReducer, applyMiddleware());
+let appStoreCreate = () => createStore(combineReducer, applyMiddleware(reduxReporter));
 
 export default appStoreCreate;
