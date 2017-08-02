6e34cbfa36999d36e8deb74fc01ad5b0e5c5d2d3
Oscar Cauich <ocauich@gravitypayments.com>
2017-08-02 14:17:14 -0700

    profile action creator added

---
+++ lab-oscar/frontend/src/action/profile-actions.js
@@ -0,0 +1,27 @@
+import superagent from 'superagent';
+
+//sync action creators
+
+export const profileCreate = (profile) => ({
+  type: 'PROFILE_CREATE',
+  payload: profile,
+});
+
+export const profileUpdate = (profile) => ({
+  type: 'PROFILE_UPDATE',
+  payload: profile,
+});
+
+// async actions creators
+
+export const profileCreateRequest = (profile) => (dispatch, getState) => {
+  let {auth} = getState();
+  return superagent.post(`${__API_URL__}/profiles`)
+    .set('Authorization', `Bearer ${auth}`)
+    .field('bio', profile.bio)
+    .attach('avatar', profile.avatar)
+    .then(res => {
+      dispatch(profileCreate(res.body));
+      return res;
+    });
+};
