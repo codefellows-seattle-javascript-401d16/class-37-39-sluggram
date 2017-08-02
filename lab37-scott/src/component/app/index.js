4b7c97707a8b70055f5acbc47e93c733828dc7b4
Scott <sdmccoy@gmail.com>
2017-08-01 21:01:56 -0700

    app and dash hello world

--- lab37-scott/src/component/app/index.js
+++ lab37-scott/src/component/app/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Dashboard from '../dashboard';
 
 class App extends React.Component{
+
   render(){
     return(
-      <div className='app'>
-        Hello Dingo
-      </div>
+      <Provider>
+        <BrowserRouter>
+          <div className='app'>
+            Hello App
+            <Route exact path='/' component={Dashboard} />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
---
+++ lab37-scott/src/component/dashboard/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Dashboard extends React.Component{
+  render(){
+    return(
+      <div className='dashboard'>
+        <main>
+          Hey from dash
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
