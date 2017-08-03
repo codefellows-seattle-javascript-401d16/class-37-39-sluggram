ccc8004ebdd72ea644babb53dd7cd9f6dd48f2de
Scott <sdmccoy@gmail.com>
2017-08-02 21:06:18 -0700

    handle signup and login are built

--- lab37-scott/src/component/landing/index.js
+++ lab37-scott/src/component/landing/index.js
@@ -1,16 +1,47 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Redirect} from 'react-router-dom';
 import AuthForm from '../auth/auth-form';
 import * as authAction from '../../action/auth-action.js';
 
 class Landing extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleSignup = this.handleSignup.bind(this);
+  }
+
+  handleLogin(user){
+    return this.props.login(user)
+      .then(() => {
+        this.props.history.push('/dashboard');
+      })
+      .catch(console.error);
+  }
+
+  handleSignup(user){
+    return this.props.signup(user)
+      .then(() => {
+        this.props.history.push('/dashboard');
+      })
+      .catch(console.error);
+  }
+
   render(){
+    //use match to get the url params. set it as params
+    console.log('match: ', this.props.match);
+    let {params} = this.props.match;
+    //make a ternary that sets handlecomplete to either the login or signup handlers
+    //based on the auth params in url
+    let handleComplete = params.auth === 'login' ? this.handleLogin : this.handleSignup;
+
     return(
       <div className='landing'>
         <main>
           Hey from Landing
           <AuthForm
-            buttonText='Sign-up'
+            auth={params.auth}
             onComplete={this.props.signup}
           />
         </main>
