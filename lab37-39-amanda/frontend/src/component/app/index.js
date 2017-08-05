1789ca698e5a74653161a1dd5092430d0ac9df5a
Amanda Koster <amandakoster@gmail.com>
2017-08-04 17:06:13 -0700

    adding a photo to a profile works

--- lab37-39-amanda/frontend/src/component/app/index.js
+++ lab37-39-amanda/frontend/src/component/app/index.js
@@ -5,24 +5,15 @@ import {BrowserRouter, Route, Link} from 'react-router-dom'
 import * as util from '../../lib/util.js'
 import {tokenSet} from '../../action/auth-actions'
 import appStoreCreate from '../../lib/app-store-create.js'
-import DashboardContainer from '../dashboard-container'
+import SettingsContainer from '../settings-container'
 import LandingContainer from '../landing-container'
 
-// let store = appStoreCreate()
-// where did Provider go?
 
 class App extends React.Component {
-  componentDidMount() {
-    let token = util.readCookie('X-Token')
-    if(token){
-      this.props.tokenSet(token)
-    }
-  }
 
   render() {
     return(
       <div className='app'>
-
         <BrowserRouter>
           <div>
             <header>
@@ -31,12 +22,12 @@ class App extends React.Component {
                 <ul>
                   <li><Link to='/welcome/signup'> signup </Link></li>
                   <li> <Link to='/welcome/login'> login </Link></li>
-                  <li><Link to='/dashboard'> dashboard </Link></li>
+                  <li><Link to='/settings'> settings </Link></li>
                 </ul>
               </nav>
             </header>
             <Route exact path='/welcome/:auth' component={LandingContainer} />
-            Route exact path='/dashboard' component={DashboardContainer}
+            <Route exact path='/settings' component={SettingsContainer} />
           </div>
         </BrowserRouter>
       </div>
--- lab37-39-amanda/frontend/src/component/dashboard-container/index.js
+++ lab37-39-amanda/frontend/src/component/dashboard-container/index.js
@@ -1,1 +1,1 @@
-DashboardContainer
+
