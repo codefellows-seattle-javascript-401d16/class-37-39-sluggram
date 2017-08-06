eaa46695267e193d9fa73deb2d785fc5b3acd7da
derekcfarmer <derek.farmer@gmail.com>
2017-08-06 16:04:51 -0700

    wrote settings container

--- lab-derek/frontend/src/component/settings-container/index.js
+++ lab-derek/frontend/src/component/settings-container/index.js
@@ -3,3 +3,59 @@ import {connect} from 'react-redux';
 import ProfileForm from '../profile-form';
 import * as util from '../../lib/util.js';
 import {profileCreateRequest, profileUpdateRequest} from '../../actions/profile-actions.js';
+
+class SettingsContainer extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.handleProfileCreate = this.handleProfileCreate.bind(this);
+    this.handleProfileUpdate = this.handleProfileUpdate.bind(this);
+  }
+
+  handleProfileCreate(profile){
+    return this.props.profileCreate(profile)
+      .then(res => {
+        this.props.history.push('/dashboard');
+      })
+      .catch(console.error);
+  }
+
+  handleProfileUpdate(profile){
+    return this.props.profileUpdate(profile)
+      .then(res => {
+        console.log('profileUpdate res', res);
+      })
+      .catch(console.error);
+  }
+
+  render(){
+    let handleComplete = this.props.profile
+      ? this.handleProfileCreate
+      : this.handleProfileUpdate;
+
+    return (
+      <div className='settings-container'>
+        <h2> settings </h2>
+
+        <ProfileForm
+          buttonText='create profile'
+          onComplete='this.handleProfileCreate'
+        />
+
+      </div>
+    );
+  }
+}
+
+let mapStateToProps = (state) => ({
+  profile: state.profile,
+});
+
+let mapDispatchToProps = (dispatch) => ({
+  profileCreate: (profile) => dispatch(profileCreateRequest(profile)),
+  profileUpdate: (profile) => dispatch(profileUpdateRequest(profile)),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps)(SettingsContainer);
