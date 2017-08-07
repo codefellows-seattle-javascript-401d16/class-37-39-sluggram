5996d41131aecad8c96cd7e31e3c6f9998e389e8
Chris Janousek <janousek77@gmail.com>
2017-08-06 21:39:46 -0700

    tests added and tweaked to get working

--- lab-chris/frontend/src/__test__/avatar.test.js
+++ lab-chris/frontend/src/__test__/avatar.test.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import {mount} from 'enzyme'
-import Avatar from '../component/avatar/'
+import Avatar from '../component/avatar'
 
 describe('Avatar', () => {
-  test('should set image src to profile avatar', () => {
-    let mockProfile = { avatar: 'yep.jpg' }
+  test('should set img source to profile avatar', () => {
+    let mockProfile = { avatar: '/cool.jpg' }
     let wrapper = mount(<Avatar profile={mockProfile} />)
-    expect(wrapper.html()).toEqual('<img class="avatar" src="/yep.jpg">')
+    expect(wrapper.html()).toEqual('<div class="avatar"><img src="/cool.jpg"></div>')
   })
 })
